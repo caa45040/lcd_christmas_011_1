390ac56fa69c48d8770a196f56cc25897d897455
caa45040 <caa45040@pop06.odn.ne.jp>
2020-11-15 23:16:33 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,6 @@
 //*         *       *    *
 //********   *****  *****
 
-//#define led1   PA8
-//#define led2   PA12
-//#define led3   PA0
 ////#define swdclk PA14
 ////#define swdio  PA13
 
@@ -17,21 +14,16 @@
 
 #define swdclk PA8
 #define swdio  PA12
-#define en     PB7
-
-//#define in7 PB7
+#define en     PA0
+#define in7    PB7
 
 void setup() {
   delay(3000);
-  // put your setup code here, to run once:
-  ////pinMode(led1,OUTPUT);
-  ////pinMode(led2,OUTPUT);
-  //pinMode(led3,OUTPUT);
 
-  pinMode(swdclk,OUTPUT);
-  pinMode(swdio,OUTPUT);
-  pinMode(en,OUTPUT);
-  //pinMode(in7,INPUT);
+    pinMode(swdclk,OUTPUT);
+    pinMode(swdio,OUTPUT);
+    pinMode(en,OUTPUT);
+    pinMode(in7,INPUT_PULLUP);
 
     DW(en,0);
     DW(swdio,0);
@@ -41,9 +33,6 @@ void setup() {
         DW(swdclk,0);
         delay(15);
     }
-
-
-
 }
 
 int v;
@@ -58,13 +47,17 @@ void seg1()
     DW(swdio,(v>>2)&1);DW(swdclk,1);DW(swdclk,0); //2
     DW(swdio,(v>>1)&1);DW(swdclk,1);DW(swdclk,0); //1
     DW(swdio,(v>>0)&1);DW(swdclk,1);DW(swdclk,0); //0
-    DW(swdio,rs);
-    //delay(500);
-    DW(en,1); delayMicroseconds(40);
-    DW(en,0); //delay(500);
+    DW(swdio,rs);   //delay(500);
+    DW(en,1);       delayMicroseconds(40);
+    DW(en,0);       //delay(500);
 }
 
-//int a;
+int a;
+int b;
+int c;
+int d;
+//int e;
+int f;
 
 int bl;
 int bh;
@@ -74,110 +67,101 @@ int s;
 int cursor1 = 0;
 
 void loop() {
-    //a=digitalRead(in7);
-    ////digitalWrite(led1,1);
-    ////digitalWrite(led2,0);
-    //digitalWrite(led3,1);
-    //DW(swdio,1);DW(swdclk,1);DW(swdclk,0);
 
     //ファンクションセット1
     rs=0;
     v=0x30;seg1();
-    delay(5);
-    //delay(1000); 
+    delay(5);   //delay(1000); 
 
     //ファンクションセット2
     rs=0;
     v=0x30;seg1();
-    delay(1);
-    //delay(1000); 
+    delay(1);   //delay(1000); 
 
     //ファンクションセット3
     rs=0;
-    v=0x30;seg1();
-    //delay(1000); 
+    v=0x30;seg1();   //delay(1000); 
 
     //ファンクションセット 2ラインモード
     rs=0;
-    v=0x38;seg1();
-    //delay(1000); 
+    v=0x38;seg1();   //delay(1000); 
 
     //表示オフ
     rs=0;
-    v=0x08;seg1();
-    //delay(1000); 
+    v=0x08;seg1();   //delay(1000); 
 
     //画面クリア
     rs=0;
     v=0x01;seg1();
-    delay(2);
-    //delay(1000); 
+    delay(2);        //delay(1000); 
 
     //エントリーモードセット
     rs=0;
-    v=0x06;seg1();
-    //delay(1000); 
+    v=0x06;seg1();    //delay(1000); 
 
     //表示オン
     rs=0;
-    v=0x08+0x04;seg1();
-    //delay(1000); 
+    v=0x08+0x04;seg1();//delay(1000); 
 
 
-//    //@文字の表示
-//    rs=1;
-//    v='@';seg1();
-//    delay(1000); 
+    //@文字の表示
+    rs=1;
+    v='@';seg1();
+    delay(1000); 
 
- //   //A文字の表示
- //   rs=1;
- //   v='A';seg1();
- //   delay(1000); 
+    //1ライン目にカーソルを移動
+    rs=0;
+    v=0x80+0x00;seg1();
+    delay(1000); 
 
+    cursor1 = 0;
+    while(1){
 
-//delay(4000); 
-//while(1){}
+        while( digitalRead(in7) == 1 ) {}
+        delay(35);
+        a=!(digitalRead(in7));
+        delay(35);
+        b=!(digitalRead(in7));
+        delay(35);
+        c=!(digitalRead(in7));
+        delay(35);
+        d=!(digitalRead(in7));
+        delay(35);
 
-//   s = 0;
+        //e=!(digitalRead(in7)); // 5 bit
+        delay(35);
+        f=!(digitalRead(in7));   // 6 bit
+        delay(35);
 
+        s=f*16+d*8+c*4+b*2+a;
 
+        if( s==31 ) {
+            //画面クリア
+            rs=0;
+            v=0x01;seg1();
+            delay(2);
+            //delay(1000); 
+            //カーソルのクリア
+            cursor1 = 0;
+        } else {
+            //文字の表示
+            v=s+'@';rs=1;seg1();cursor1++;
+            //delay(300); 
+        }
 
-//delay(2000);
-
-//digitalWrite(swdio,0);
-cursor1 = 0;
-while(1){
-
-    //O文字の表示
-    v=cursor1+'@'; /*'O'*/;rs=1;seg1();cursor1++;
-    delay(300); 
-
-    if(cursor1 == 8){
-        //2ライン目にカーソルを移動
-        rs=0;
-        v=0x80+0x40;seg1();
-        //delay(1000); 
-    } else if(cursor1 >= 16) {
-        //画面クリア
-        rs=0;
-        v=0x01;seg1();
-        delay(2);
-        //delay(1000); 
-        //カーソルのクリア
-        cursor1 = 0;
-    }
-
-    //digitalWrite(swdclk,1);
-    ///digitalWrite(swdio,1);
-    //delay(2000);
-
-    ////digitalWrite(led1,0);
-    ////digitalWrite(led2,1);
-    //digitalWrite(led3,0);
-
-    //digitalWrite(swdclk,0);
-    ///digitalWrite(swdio,0);
-    //delay(2000);
-}
-
-}
+        if(cursor1 == 8){
+            //2ライン目にカーソルを移動
+           rs=0;
+           v=0x80+0x40;seg1();
+           //delay(1000); 
+        } else if(cursor1 >= 16) {
+            //画面クリア
+            rs=0;
+            v=0x01;seg1();
+            delay(2);
+            //delay(1000); 
+            //カーソルのクリア
+            cursor1 = 0;
+        } //endif
+    } //while
+} //end loop
